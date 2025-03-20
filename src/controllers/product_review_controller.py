dcda2f0aad0f714c12cc1a44a722a7025c74184c
samuelindrawijaya <samuelindraw75@gmail.com>
2025-03-20 23:18:18 +0700

    add controller get_product_reviews_all

--- src/controllers/product_review_controller.py
+++ src/controllers/product_review_controller.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+from src.services.review_service import (
+    get_product_reviews,
+)
+
 @router.get("/product-review/{product_review_id}")
-async def get_product_review(product_review_id: int):
-    return await {"product_review_id": product_review_id}
+async def get_product_reviews_all(product_review_id: int):
+    return await get_product_reviews(product_review_id)
