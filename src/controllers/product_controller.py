64e5740401dbe11bbd3e0d971437ac9fade5a4b9
kingahmadr <ahmadnromiz@gmail.com>
2025-03-20 17:21:42 +0700

    get product by id

--- src/controllers/product_controller.py
+++ src/controllers/product_controller.py
@@ -3,12 +3,13 @@ from fastapi import APIRouter
 router = APIRouter()
 
 from src.services.product_service import (
-    get_all_products
+    get_all_products,
+    get_product_by_id
 )
 
 @router.get("/products")
 async def get_all_products_controller():
     return await get_all_products()
 @router.get("/product/{product_id}")
-async def get_product(product_id: int):
-    return await {"product_id": product_id}
+async def get_product_by_id_controller(product_id: int):
+    return await get_product_by_id(product_id)
--- src/services/product_service.py
+++ src/services/product_service.py
@@ -17,3 +17,12 @@ async def get_all_products():
     except SQLAlchemyError as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
     
+async def get_product_by_id(product_id: int):
+    try:
+        product = db.session.query(Product).get(product_id)
+        if product:
+            return JSONResponse(content={"data": product.to_dict()}, status_code=200)
+        else:
+            return JSONResponse(content={"error": "Product not found"}, status_code=404)
+    except SQLAlchemyError as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
