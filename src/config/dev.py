8ea1979068bd85fdfe1ae5017e20a1a5784687b4
kingahmadr <ahmadnromiz@gmail.com>
2025-03-21 23:13:04 +0700

    change prod config

--- src/config/dev.py
+++ src/config/dev.py
@@ -7,14 +7,14 @@ from pydantic import Field
 load_dotenv(override=True)
 
 class DevConfig(BaseConfig):
-    # DATABASE_URL: str = os.getenv("DB_DEV_URI")
-    DATABASE_URL: str = Field(..., alias="DB_URI_DEV")
+    DATABASE_URL: str = os.getenv("DB_DEV_URI")
+    # DATABASE_URL: str = Field(..., alias="DB_URI_DEV")
     # JWT_SECRET: str = os.getenv("JWT_SECRET")
     # Use ClassVar to indicate these are not Pydantic model fields
     ALGORITHM: ClassVar[str] = "HS512"
     ACCESS_TOKEN_EXPIRE_MINUTES: ClassVar[int] = 30
-    # SECRET_KEY: str = os.getenv("KEY_SECRET")
-    SECRET_KEY: str = Field(..., alias="KEY_SECRET")
+    SECRET_KEY: str = os.getenv("KEY_SECRET")
+    # SECRET_KEY: str = Field(..., alias="KEY_SECRET")
 
     # Define allowed origins
     origins: ClassVar[List[str]] = [
