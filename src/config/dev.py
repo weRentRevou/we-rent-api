e28ef502bd395e88593c31ccb5fb2e36e1641f07
kingahmadr <ahmadnromiz@gmail.com>
2025-03-21 20:23:33 +0700

    comment the JWT_SECRET

--- src/config/dev.py
+++ src/config/dev.py
@@ -9,11 +9,11 @@ load_dotenv(override=True)
 class DevConfig(BaseConfig):
     # DATABASE_URL: str = os.getenv("DB_DEV_URI")
     DATABASE_URL: str = Field(..., alias="DB_URI_DEV")
-    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    # JWT_SECRET: str = os.getenv("JWT_SECRET")
     # Use ClassVar to indicate these are not Pydantic model fields
     ALGORITHM: ClassVar[str] = "HS512"
     ACCESS_TOKEN_EXPIRE_MINUTES: ClassVar[int] = 30
-    SECRET_KEY: str = os.getenv("KEY_SECRET")
+    # SECRET_KEY: str = os.getenv("KEY_SECRET")
 
     # Define allowed origins
     origins: ClassVar[List[str]] = [
